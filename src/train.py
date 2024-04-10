e0a04f13ef06786e92385aa2d83e8f60854a530c
MarioFreireArias <mario.freire.arias@gmail.com>
2024-04-10 13:36:13 +0200

    Creted/Fixed module to train the rnn model.

--- src/train.py
+++ src/train.py
@@ -1,36 +1,40 @@
-# deep learning libraries
+# import necessary dependencies
 import torch
+# from torch import nn, optim
 from torch.utils.data import DataLoader
+
 from torch.utils.tensorboard import SummaryWriter
 
-# other libraries
-# from tqdm.auto import tqdm
-from typing import Final
+from typing import Final, Dict  # , Tuple
 
-# own modules
-from src.data import load_data
+from src.utils import save_model, set_seed, plot_accuracies
 from src.models import RNNModel
-from src.train_functions import train_step, val_step
-from src.utils import set_seed, save_model
+from src.data import load_data
+from src.train_functions import train_torch_model
 
-# static variables
-DATA_PATH: Final[str] = "data"
+from gensim.models.keyedvectors import load_word2vec_format
 
-# set device and seed
-device = torch.device(
-    "cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-print(f"Using Device: {device}")
+SEED = 2222
+set_seed(SEED)
 
-set_seed(42)
+DATA_PATH: Final[str] = "./NLP_Data/data"
 
-# HYPERPARAMETER INITIALIZATION
-HIDDEN_SIZE: int = 32
-LEARNING_RATE: float = 7.5e-4
-EPOCHS: int = 200
-BATCH_SIZE: int = 64
-PATIENCE: int = 40
-LR_DECREASE_FACTOR: float = 0.1  # new_lr = lr * factor
+
+batch_size: int = 64
+epochs: int = 40
+print_every: int = 5
+patience: int = 20
+learning_rate: float = 5e-4
+hidden_dim: int = 256
+num_layers: int = 2
+
+model_name = f"rnn_hidden_{hidden_dim}_lr_{learning_rate}_epochs_{epochs}" + \
+    f"_batch_{batch_size}_patience_{patience}"
+
+# TODO: Check if GPU is available and move the model to GPU
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f"Training on {device}")
 
 
 def main() -> None:
@@ -39,76 +43,74 @@ def main() -> None:
     """
 
     # TODO
-
+    print("Loading data...")
     # load the data
     train_dataloader: DataLoader
     val_dataloader: DataLoader
     test_dataloader: DataLoader
-    mean_price: float
-    std_price: float
     (
         train_dataloader,
         val_dataloader,
-        test_dataloader,
-        mean_price,
-        std_price
+        test_dataloader
     ) = load_data(save_path=DATA_PATH)
 
-    # initialize the model
-    model = RNNModel(HIDDEN_SIZE).to(device)
+    print("Loading embeddings...")
 
+    w2v_model = load_word2vec_format(
+        "./NLP_Data/embeddings/GoogleNews-vectors-negative300.bin.gz", binary=True)
+    embedding_weights = torch.FloatTensor(w2v_model.vectors)
+
+    print("Initializing model...")
+    # intialize the model
+    model = RNNModel(embedding_weights, hidden_dim, num_layers).to(device)
+
+    print("Initializing optimizer and loss function...")
     # initialize the optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    # initialize the loss function (MAE)
-    loss_fn = torch.nn.L1Loss()
+    # initialize the loss function (BCE with logits loss)
+    loss_fn = torch.nn.BCEWithLogitsLoss()
 
     # initialize the summary writer
     writer = SummaryWriter()
 
-    # initialise the scheduler
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer, mode="min", patience=PATIENCE, factor=LR_DECREASE_FACTOR, verbose=True
+    train_accuracies: Dict[int, float]
+    val_accuracies: Dict[int, float]
+
+    print("STARTING TRAINING PROCESS...")
+    # train the model
+    (
+        train_accuracies,
+        val_accuracies
+    ) = train_torch_model(
+        model=model,
+        train_dataloader=train_dataloader,
+        val_dataloader=val_dataloader,
+        criterion=loss_fn,
+        optimizer=optimizer,
+        epochs=epochs,
+        print_every=print_every,
+        patience=patience,
+        writer=writer,
+        device=device
     )
 
-    # train the model
-    for epoch in range(EPOCHS):
-        print(f"TRAINING: Epoch {epoch} / {EPOCHS}")
-
-        # train the model
-        train_step(model,
-                   train_dataloader,
-                   mean_price,
-                   std_price,
-                   loss_fn,
-                   optimizer,
-                   writer,
-                   epoch,
-                   device)
-
-        # validate the model
-        val_step(model,
-                 val_dataloader,
-                 mean_price,
-                 std_price,
-                 loss_fn,
-                 scheduler,
-                 writer,
-                 epoch,
-                 device)
-
     print("Saving model...")
-    # save the model
-    # delete ./models/best_model.pt to save the new model otherwise it doesnt update it)
-    try:
-        import os
-        os.remove("models/best_model.pt")
-    except Exception:
-        pass
 
+    # save the model
     save_model(model, "best_model")
-    save_model(model, f"hidden_{HIDDEN_SIZE}_lr_{LEARNING_RATE}_epochs_{EPOCHS}" +
-               f"_batch_{BATCH_SIZE}_patience_{PATIENCE}")
+    save_model(model, model_name)
+
+    print("Plotting accuracies...")
+    # plot the accuracies
+    plot_accuracies(model,
+                    train_dataloader,
+                    val_dataloader,
+                    test_dataloader,
+                    train_accuracies,
+                    val_accuracies,
+                    model_name,
+                    device=device)
 
     return None
 
