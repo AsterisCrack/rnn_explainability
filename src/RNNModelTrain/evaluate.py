ea663373615f2ba50382debb9341fece4e23b937
Asteris <94953985+AsterisCrack@users.noreply.github.com>
2024-04-12 12:04:44 +0200

    fixed evaluate after merge

--- src/RNNModelTrain/evaluate.py
+++ src/RNNModelTrain/evaluate.py
@@ -1,60 +1,39 @@
-# deep learning libraries
 import torch
-from torch.utils.data import DataLoader
-from torch.jit import RecursiveScriptModule
 
-# other libraries
+from .train_functions import t_step
+
+from .utils import load_model, set_seed
+
+from .data import load_data
+
 from typing import Final
 
-# own modules
-from src.RNNModelTrain.data import load_data
-from src.RNNModelTrain.utils import set_seed
-from src.RNNModelTrain.train_functions import t_step
 
-# static variables
-DATA_PATH: Final[str] = "data"
-NUM_CLASSES: Final[int] = 10
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# set device
-device = torch.device(
-    "cuda") if torch.cuda.is_available() else torch.device("cpu")
-set_seed(42)
+SEED = 2222
+set_seed(SEED)
+
+DATA_PATH: Final[str] = "./NLP_Data/data"
 
 
 def main() -> None:
-    """
-    This function is the main program.
-    """
-
-    # TODO
+    # load model
+    print("Loading model...")
+    model = load_model("best_model")
 
     # load the data
-    train_dataloader: DataLoader
-    val_dataloader: DataLoader
-    test_dataloader: DataLoader
-    mean_price: float
-    std_price: float
-    (
-        train_dataloader,
-        val_dataloader,
-        test_dataloader,
-        mean_price,
-        std_price
-    ) = load_data(save_path=DATA_PATH)
+    print("Loading data...")
+    train_dataloader, val_dataloader, test_dataloader = load_data(
+        save_path=DATA_PATH)
 
-    # load the model
-    model: RecursiveScriptModule = torch.jit.load("models/best_model.pt")
+    # perform testing
+    print("Testing model...")
+    test_accuracy = t_step(model, test_dataloader, device=DEVICE)
 
-    # TODO: print the parameters of the model, and the shape of the fc layer
+    print(f"Test accuracy: {test_accuracy:.4f}")
 
-    # evaluate the model
-    test_results = t_step(model, test_dataloader,
-                          mean_price, std_price, device)
-
-    # print the test resulsts
-    print(f"MAE test loss: {test_results}")
-
-    return None
+    return
 
 
 if __name__ == "__main__":
